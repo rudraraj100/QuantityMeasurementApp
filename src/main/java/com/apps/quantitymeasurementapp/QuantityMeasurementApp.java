03eda4241b30fd6c3ce1f88103620f61c9e639fa
rudraraj100 <rudrar2002@gmail.com>
2026-02-20 17:30:57 +0530

    [Rudra Raj] : Add init set up

---
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -0,0 +1,16 @@
+package com.apps.quantitymeasurementapp;
+
+public class QuantityMeasurementApp {
+
+	public static void main(String[] args) {
+		
+		Feet f1 = new Feet(2.5);
+		
+		Feet f2 = new Feet(2.5);
+		
+		System.out.println("Equals: " + f1.equals(f2)); 
+		
+
+	}
+
+}

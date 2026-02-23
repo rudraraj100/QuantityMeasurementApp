db05af02a5a7badb72aa4086f88144385e2fe989
rudraraj100 <rudrar2002@gmail.com>
2026-02-23 16:22:19 +0530

    [Rudra Raj] : Added UC10 Generic Quantity Class With Unit Interface files

---
+++ src/main/java/com/apps/quantitymeasurementapp/IMeasurable.java
@@ -0,0 +1,13 @@
+
+package com.apps.quantitymeasurementapp;
+
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    String getUnitName();
+}

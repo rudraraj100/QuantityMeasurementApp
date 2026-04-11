67bd16e11e19a364858827f3d7a7ca52286286f8
rudraraj100 <rudrar2002@gmail.com>
2026-04-11 13:00:36 +0530

    [Rudra Raj] : Added cors config file updation

--- src/main/java/com/app/quantitymeasurement/config/CorsConfig.java
+++ src/main/java/com/app/quantitymeasurement/config/CorsConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+@Bean
+WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**") // allow all endpoints
+                    .allowedOrigins(
+                            "http://localhost:4200", // for local
+                            "https://qma-frontend-production.up.railway.app" // for Railway
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+    };
 }
